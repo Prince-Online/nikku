f0acc2dcca05a6cdea0ffe45794ad1d75407c8da
Prince-Online <145020320+Prince-Online@users.noreply.github.com>
2025-01-26 10:54:36 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,35 @@
+if (localStorage.getItem("nikku_local")) {
+  window.location.href = "index.html";
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+  var url = "https://script.google.com/macros/s/AKfycbzMMCrJliDav6OZofgSDz53zIk74rO8I0YVciZriCya9iKvgbxkPB4ylW4JZQjPob6J/exec";
+
+  var data = {
+    "username": username,
+    "password": password
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(data)
+  })
+  .then(response => response.text())
+  .then(responseText => {
+    if (responseText === "success") {
+      localStorage.setItem("nikku_local", username);
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid username or password!");
+    }
+  })
+  .catch(error => console.error('Error:', error));
+});
